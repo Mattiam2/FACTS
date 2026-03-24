1fc1828097e3b64de91c7c0919317a7f34a906e0
Mattiam2 <mattia.maglie@studenti.unipd.it>
2026-03-24 19:08:23 +0100

    Refactor: update utility route from `/create_vp` to `/create_vc` and add TODO for simulator testability

--- ebsi_sim/api/utils.py
+++ ebsi_sim/api/utils.py
@@ -1,8 +1,11 @@
-"""
-from fastapi import Response, APIRouter, Depends, HTTPException
+from fastapi import APIRouter
 
 router = APIRouter(prefix="/utils", tags=["utils"])
 
-@router.get("/create_vp")
+
+@router.get("/create_vc")
 def get_vc(did):
-"""
+    # TODO: Build a VC, and return it signed by a DID signer trusting the client whatever.
+    # This is necessary in order to make the simulator testable
+    # Otherwise I would need to build TAO API which is out of the scope
+    pass
